5f24811a0bf55389c997f635a2927a79680e477b
Erik Öberg <erik.oberg@miun.se>
2021-11-03 15:02:42 +0100

    Efter föreläsning 3 - fixat poängräkning i Yatzy - Vi kontrollerar bonus i spelet - tittat på if-satser i projektet FL3

---
+++ FL3/MainWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FL3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int count=0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOk_Click(object sender, RoutedEventArgs e)
+        {
+           
+            // tilldelar jag variabeln värdet 1
+            //count = 1;
+
+            // men jag vill ju öka med ett i taget
+
+            //count = count + 1;
+            
+            
+            count++;
+
+            /*
+            // Ännu ett sätt att räkna 
+            // operator +, -, /, *
+            count += 3;
+            count -= 3;
+            count /= 3;
+            count *= 3;
+            */
+            MessageBox.Show(count.ToString());
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            // if-sats
+            int score = 11, bonus, age=10;
+
+
+            if (age <= 12) // jag är ett barn
+            {
+                if (score >= 10)
+                {
+                    bonus = 20;
+                }
+                else
+                {
+                    bonus = 0;
+                }
+            }
+            else // jag är vuxen
+            {
+                if (score >= 20)
+                {
+                    bonus = 20;
+                }
+                else
+                {
+                    bonus = 0;
+                }
+            }
+
+
+
+
+            /*
+            if(score >= 20)
+            {
+                bonus = 40;
+            }
+            else if (score >= 15)
+            {
+                bonus = 30;
+            }
+            else if(score >= 10)
+            {
+                bonus = 20;
+            }
+            else
+            {
+                bonus = 0;
+            }*/
+            /*
+            if(score >= 0 && score <10)
+            {
+                bonus = 0;
+            }
+            */
+
+
+            
+            MessageBox.Show($"Grattis, du fick {bonus} extra poäng");
+
+            // if else
+        }
+    }
+}
--- Yahtzee/MainWindow.xaml.cs
+++ Yahtzee/MainWindow.xaml.cs
@@ -20,6 +20,7 @@ namespace Yahtzee
     /// </summary>
     public partial class MainWindow : Window
     {
+        int total = 0, bonus=0;
         public MainWindow()
         {
             InitializeComponent();
@@ -28,7 +29,7 @@ namespace Yahtzee
         private void btnSave_Click(object sender, RoutedEventArgs e)
         {
             // deklarerar variabler av datatypen integer = heltal
-            int ones, twos, threes, fours, fives, sixes, total=0;
+            int ones, twos, threes, fours, fives, sixes;
 
             // hämta värdet från textboxen och lagra i variabel
 
@@ -41,9 +42,45 @@ namespace Yahtzee
             // beräkna totalsumman
             total = ones + twos;
 
+            
+            
+        }
+
+        private void btnOnes_Click(object sender, RoutedEventArgs e)
+        {
+            int ones;
+            ones = int.Parse(txtOnes.Text);
+            // hämtar värdet från total
+            // och ökar sedan detta värde med vår poäng för ettor
+            total += ones;
+
+            // presentera värdet till scorekortet
+
+            // Undersök om totalpoängen ger mig bonus
+            // Vi låtsas: Bonus om total är 10 poäng eller mer
+            // --> bonus = 50p
+            // annars bonus 0p
+
+            // vi gör en if-sats
+            // inom parentes sätter vi ett villkor
+            // detta måste valideras som sant eller falskt
+            if (total >= 10)
+            {
+                bonus = 50;
+                total += bonus;
+                txtBonus.Text = bonus.ToString();
+            }
+            txtTotal.Text = total.ToString();
+        }
+
+        private void btnTwos_Click(object sender, RoutedEventArgs e)
+        {
+            int score;
+            score = int.Parse(txtTwos.Text);
+            total += score;
+
             // presentera värdet till scorekortet
             txtTotal.Text = total.ToString();
-            
         }
     }
 }
